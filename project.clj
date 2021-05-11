48090955970fa7c3f5881d8830af5d27716fbe4e
Shane Mulligan <mullikine@gmail.com>
2021-05-11 15:53:57 +1200

    11.05.21

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [clj-commons/clj-yaml "0.7.106"]
+                 [org.clojure/tools.trace "0.7.11"]]
   :main ^:skip-aot mustache-transform.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
